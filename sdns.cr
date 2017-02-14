439498eaf295592afb39af3931cd3c357bbdbb34
Chris Cummer <chris@unbounce.com>
2017-02-13 17:35:46 -0800

    Displays version with --version

--- sdns.cr
+++ sdns.cr
@@ -16,20 +16,19 @@ cli = Commander::Command.new do |cmd|
   end
 
   cmd.commands.add do |cmd|
-    cmd.use = "list"
-    cmd.short = "List the available DNS sets."
-    cmd.long = cmd.short
-    cmd.run do |opts, args|
-      DnsList.new.list
-    end
-  end
-
-  cmd.commands.add do |cmd|
     cmd.use = "default"
     cmd.short = "Reset to the default DNS servers."
     cmd.long = cmd.short
     cmd.run do |opts, args|
       DnsList.new.default
+    end
+  end
+  cmd.commands.add do |cmd|
+    cmd.use = "list"
+    cmd.short = "List the available DNS sets."
+    cmd.long = cmd.short
+    cmd.run do |opts, args|
+      DnsList.new.list
     end
   end
 
@@ -40,6 +39,15 @@ cli = Commander::Command.new do |cmd|
     cmd.run do |opts, args|
       raise "Must provide a key value" if args.empty?
       DnsList.new.switch(args[0])
+    end
+  end
+
+  cmd.commands.add do |cmd|
+    cmd.use = "--version"
+    cmd.short = "Displays the version."
+    cmd.long = cmd.short
+    cmd.run do |opts, args|
+      puts "0.0.1"
     end
   end
 end
